093bbbf912898656f4ddd1da70bec60f3c08679c
Danjune <daniel.ayala621@gmail.com>
2024-05-21 20:44:35 -0600

    añadir randoms porque si se define una estatica dice "Siempre True/False"

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 
-
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         //if practice
 
-        //Numeros
-        int num1 = 45;
-        int num2 = 35;
+        Random random = new Random();
+        int num1 = random.nextInt(100);
+        int num2 = random.nextInt(100);
+        //Numeros random
+        //int num1 = 45;
+        //int num2 = 35;
         //Operacion
         if (num1 > num2){
             System.out.println(num1 + " es mayor a " + num2);
