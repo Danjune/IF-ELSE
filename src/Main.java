e0546caf45f364ff8e04406c702dd2fd7987798e
Danjune <daniel.ayala621@gmail.com>
2024-05-21 20:39:56 -0600

    escritura del if

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //if practice
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Numeros
+        int num1 = 45;
+        int num2 = 35;
+        //Operacion
+        if (num1 > num2){
+            System.out.println(num1 + " es mayor a " + num2);
+        }else{
+            System.out.println(num2 + " es mayor a " + num1);
+
         }
+
     }
 }
